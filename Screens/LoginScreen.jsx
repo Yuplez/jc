1951b75030cb25e1708ed433e3669e52bf40243f
solomonsanthosh <solosanty2064@gmail.com>
2022-10-30 17:46:10 +0530

    SS

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,21 +1,140 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  KeyboardAvoidingView,
+} from 'react-native';
+import React, {useState} from 'react';
+import {auth} from '../../firebase/firebase';
 
-const LoginScreen = () => {
+const Login = ({navigation, setUser}) => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleLogin = async () => {
+    auth
+      .signInWithEmailAndPassword(email.trim(), password.trim())
+      .then(async userCredential => {
+        const user = userCredential.user;
+        navigation.push('')
+      })
+
+      .catch(error => {
+        const errorCode = error.code;
+        console.log(error);
+        const errorMessage = error.message;
+        setErrorMessage(errorMessage);
+      });
+  };
+
   return (
-    <View style={styles.home}>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        style={{
+          width: '100%',
+          padding: 30,
+          height: Dimensions.get('window').height,
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+        }}>
+        
+
+        
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          placeholder="Enter your email"
+          style={styles.input}
+          onChangeText={text => setEmail(text)}
+          value={email}
+          keyboardType="email-address"
+        />
+        <TextInput
+          placeholder="Enter your password"
+          style={styles.input}
+          onChangeText={text => setPassword(text)}
+          secureTextEntry={true}
+          value={password}
+        />
+        <Text>{errorMessage}</Text>
+        <View style={styles.allbutton}>
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={{color: '#50867a', fontWeight: '700'}}>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.push('Signup')}>
+            <Text style={{color: '#50867a', fontWeight: '700'}}>Sign up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.push('reset')}>
+            <Text style={{color: '#50867a', fontWeight: '700'}}>
+              Reset Password?
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
-export default LoginScreen;
-
 const styles = StyleSheet.create({
-	home: {
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: '100%',
+
+    zIndex: 100,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#dadada',
+    borderRadius: 30,
+    width: '100%',
+    marginBottom: 10,
+    padding: 15,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 30,
+    color: '#000000',
+    fontWeight: '700',
+    marginLeft: 10,
+  },
+  allbutton: {
+    // flexDirection: "row",
+    // justifyContent: "center",
+    // alignContent: "center",
+    width: '100%',
+    zIndex: 100,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#339d48',
+    padding: 20,
+    width: '100%',
+    marginTop: 20,
+    zIndex: 100,
+
+    // textAlign: 'center',
+    justifyContent: 'center',
     alignItems: 'center',
-	justifyContent: 'center',
-    backgroundColor: 'white',
-	}
+    borderRadius: 30,
+    // borderTopEndRadius: 20,
+    // borderBottomLeftRadius: 20,
+    // color: 'white'
+  },
+  text: {
+    marginTop: 10,
+    color: 'red',
+  },
 });
+
+export default Login;
--- Screens/RegisterScreen.jsx
+++ Screens/RegisterScreen.jsx
@@ -1,21 +1,136 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { auth } from "../../firebase/firebase";
 
-const RegisterScreen = () => {
+import { useNavigation } from "@react-navigation/native";
+// const auth = getAuth();
+
+
+const LoginScreen = () => {
+
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+
+  const handleLogin = async () => {
+    auth
+      .createUserWithEmailAndPassword(email.trim(), password.trim())
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+        setErrorMessage(errorMessage);
+        if (
+          errorMessage ==
+          "The email address is already in use by another account."
+        ) {
+          navigation.push("PasswordScreen");
+        }
+      });
+
+
+  };
+
   return (
-    <View style={styles.home}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        style={{
+          width: "100%",
+          height: Dimensions.get("window").height,
+          alignItems: "center",
+          justifyContent:"center"
+        }}
+      >
+       
+
+        
+
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput
+          placeholder="Enter your name"
+          style={styles.input}
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        <TextInput
+          placeholder="Enter your email"
+          style={styles.input}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          placeholder="Enter your password"
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+        <Text>{errorMessage}</Text>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={{ color: "#50867a", fontWeight: "700" }}>Sign up</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
-export default RegisterScreen;
+
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-  home: {
+  container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#dadada",
+    width: "80%",
+    borderRadius: 30,
+    marginBottom: 10,
+    padding: 15,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 50,
+    color: "#000000",
+    fontWeight: "700",
+    // marginLeft:10
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: "#339d48",
+    padding: 10,
+    width: "30%",
+    margin: 5,
+    color: "black",
+    // textAlign: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+  },
+  text: {
+    marginTop: 10,
+    color: "red",
   },
 });
